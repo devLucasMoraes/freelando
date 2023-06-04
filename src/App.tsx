fe109a10b762b9c78b9cb8f28a4937f768781a71
devLucasMoraes <devlucasmoraes@gmail.com>
2023-06-04 11:40:14 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Estilos } from "./components/EstilosGlobais/Estilos"
+
+function App() {
+
+  return (
+    <>
+      <Estilos/>
+      <h1>Freelando</h1>
+    </>
+  )
+}
+
+export default App
